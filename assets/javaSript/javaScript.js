54df288b5e05a8932c58d3166197516841919572
Tracy Tran <tracytran1189@gmail.com>
2021-12-30 22:13:42 -0600

    current weather display

--- assets/javaSript/javaScript.js
+++ assets/javaSript/javaScript.js
@@ -32,5 +32,26 @@ searchButton.onclick = (event) => {
     console.log('search', cityName);
     getCityForecast(cityName).then(data => {
         console.log('city data', data);
+
+        var currentWeather = data.oneCallResponse.current;
+        var cityName = data.currentWeatherReponse.name;
+        setCurrentWeather(currentWeather, cityName);
     });
 };
+
+function setCurrentWeather(currentWeather, cityName) {
+    var cityNameEl = document.getElementById('city-name');
+    cityNameEl.textContent = `${cityName} (${(new Date()).toLocaleDateString()})`
+
+    var tempValue = document.querySelector('#city-temp .value');
+    tempValue.textContent = `${currentWeather.temp}°F`;
+
+    var windValue = document.querySelector('#city-wind .value');
+    windValue.textContent = `${currentWeather.wind_speed} mph`
+
+    var humidityValue = document.querySelector('#city-humidity .value');
+    humidityValue.textContent = `${currentWeather.humidity}%`
+
+    var uvValue = document.querySelector('#city-uv .value');
+    uvValue.textContent = `${currentWeather.uvi}`
+}
