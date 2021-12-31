774e4e56a4794e00c0a9f4786cbe5565571b5688
Tracy Tran <tracytran1189@gmail.com>
2021-12-30 21:54:29 -0600

    added onecallapi to get UV and historial

--- assets/javaSript/javaScript.js
+++ assets/javaSript/javaScript.js
@@ -2,9 +2,24 @@ var apiKey= "0a049cb78e7e615e6af6861a230a5dd9";
 var getCityForecast = function(cityName) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
     return fetch(apiUrl).then(function(response) {
-        return response.json().then(data => data);
+        return response.json().then(data => {
+            var {lat, lon} = data.coord;
+            return getOneCallApi(lat, lon).then(oneCallResponse => {
+                return {
+                    currentWeatherReponse: data,
+                    oneCallResponse
+                }
+            });
+        });
     });
 };
+
+var getOneCallApi = function(lat, lon) {
+    var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+    return fetch(apiUrl).then(function(response) {
+        return response.json().then(data => data);
+    });
+}
 
 var searchButton = document.getElementById('search-button');
 var cityInput = document.getElementById('city');
