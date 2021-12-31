7b122b0e23894a6e57adb041c743bc8384dc0ca1
Tracy Tran <tracytran1189@gmail.com>
2021-12-30 21:29:49 -0600

    weather api integration

--- assets/javaSript/javaScript.js
+++ assets/javaSript/javaScript.js
@@ -1,9 +1,21 @@
 var apiKey= "0a049cb78e7e615e6af6861a230a5dd9";
 var getCityForecast = function(cityName) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
-    fetch(apiUrl).then(function(response) {
-        console.log("London");
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
+    return fetch(apiUrl).then(function(response) {
+        return response.json().then(data => data);
     });
 };
 
-getCityForecast();
+var searchButton = document.getElementById('search-button');
+var cityInput = document.getElementById('city');
+
+searchButton.onclick = (event) => {
+    event.preventDefault();
+
+    var cityName = cityInput.value;
+
+    console.log('search', cityName);
+    getCityForecast(cityName).then(data => {
+        console.log('city data', data);
+    });
+};
